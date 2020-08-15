a6660305a01ffe66c30bc771dbb801e2bec40470
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2020-08-15 16:42:15 -0700

    initial commit

---
+++ Infone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Infone
+//
+//  Created by Nathan Endow on 8/15/20.
+//  Copyright © 2020 Nathan Endow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

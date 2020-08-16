41f364f17ffd87109933d1c8d484177da6038f5d
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2020-08-15 17:38:22 -0700

    Basic setup in tableview for phone info

---
+++ Infone/NavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationViewController.swift
+//  Infone
+//
+//  Created by Nathan Endow on 8/15/20.
+//  Copyright © 2020 Nathan Endow. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+
+
+    override func viewDidLoad() {
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
